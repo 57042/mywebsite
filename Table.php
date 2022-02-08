b8fdd0aba70ef413b21f33c27238e15b22203b37
57042 <97938143+57042@users.noreply.github.com>
2022-02-08 16:34:58 +0700

    Create Table.php

---
+++ Table.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>kim taehyung</td>
+        <td>taehyung</td>
+        <td><img src="https://i.pinimg.com/originals/a5/b9/ae/a5b9aea7aa60f55329c2eee88e23769d.jpg" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+        <tr>
+        <td>2</td>
+        <td>jeon jungkook</td>
+        <td>jungkook</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/4/40/Jungkook_x_Samsung_Galaxy_August_2021.png" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+        <tr>
+        <td>3</td>
+        <td>park jimin</td>
+        <td>jimin</td>
+        <td><img src="https://i.pinimg.com/originals/0b/5e/26/0b5e2667842126984ddc46295f4c4934.jpg" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+        <tr>
+        <td>4</td>
+        <td>kim seokjin</td>
+        <td>seokjin</td>
+        <td><img src="https://f.ptcdn.info/655/075/000/r3l5tg8s0xwexXvIzy5-o.jpg" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+        <tr>
+        <td>5</td>
+        <td>min yoongi</td>
+        <td>yoongi</td>
+        <td><img src="https://s.isanook.com/jo/0/ud/485/2425871/suga.jpg" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+        <tr>
+        <td>6</td>
+        <td>jung hoseok</td>
+        <td>j-hope</td>
+        <td><img src="https://f.ptcdn.info/052/066/000/py4m14m9fMuQId6l09N-o.jpg" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/bts_twt">twitter</a></td>
+      </tr>
+    </table>
+  </body> 
+</html>
