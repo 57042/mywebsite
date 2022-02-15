4856bc5431a08911f85285192329eb68c7d30f1f
57042 <97938143+57042@users.noreply.github.com>
2022-02-15 14:11:20 +0700

    Create Page2.php

---
+++ Page2.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: lightblue;
+			}
+                        body{
+                                background-color: pink;
+			}
+                        h1{
+                                color: yellow;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>นางสาวสุชานาถ พรมจันทร์ ม.4/13 เลขที่23</h1>
+		</div>
+	</body>
+</html>
