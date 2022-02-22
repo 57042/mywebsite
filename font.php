3a1d3f0d9ab1188471acb01a581cea677287fbfd
57042 <97938143+57042@users.noreply.github.com>
2022-02-22 14:43:11 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(smile-Regula.woff)
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url(ChocolateteehaypaiTEST.woff)
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวสุชานาถ พรมจันทร์ ม.4/13 เลขที่ 23 เลขประจำตัว 57042</h1>
+  <h1 id="font2">นางสาวสุชานาถ พรมจันทร์ ม.4/13 เลขที่ 23 เลขประจำตัว 57042</h1>
+</body>
+</html>
